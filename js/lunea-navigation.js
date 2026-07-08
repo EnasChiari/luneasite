4ce17d52847fded097349d6dcef8b1b587342440
Enas3 <159634617+EnasChiari@users.noreply.github.com>
2026-07-08 08:14:15 -0300

    Update lunea-navigation.js

--- js/lunea-navigation.js
+++ js/lunea-navigation.js
@@ -13,7 +13,7 @@ const R = {
   produto:    'produto.html',
   produtopag: 'produtopag.html',
   busca:      'produtos.html',
-  sobrenos:   'sobrenos.html',
+  sobrenos:   'quemsomos.html',
 };
 
 const ir = rota => window.location.href = rota;
